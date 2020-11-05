bf8c1ee179d8528f40fcc578b98fcb73cd1cb9e2
Arek Dzik <arkadiusz.dzik@comarch.pl>
2020-11-05 21:15:41 +0100

    Fixes in building image

--- build_chirpimage.sh
+++ build_chirpimage.sh
@@ -3,11 +3,9 @@
 # build_chirpimage.sh 
 
 ctr=$(buildah from fedora:31)
-buildah run $ctr /bin/sh -c \
- 'dnf -y update \
-  dnf -y install chirp \
-  dnf clean all' 
+
+buildah run $ctr /bin/sh -c 'dnf install -y chirp'
 
 buildah config --cmd "/usr/bin/chirpw" $ctr
 
-buildah commit $ctr localhost/chirp:fedora31_20201103
+buildah commit $ctr localhost/chirp:fedora31_20201105
--- run_chirp.sh
+++ run_chirp.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
 str_image_name=localhost/chirp:fedora31_20201103
+str_chirp_device="/dev/ttyUSB0"
 
-# --volume /dev/ttyUSB0:/dev/ttyUSB0
+if [ ! -e "$str_chirp_device" ]
+then
+  echo "Expecting a $str_chirp_device device. Is cable connected?"
+  exit 1
+fi
+if [ "$(stat -c %a $str_chirp_device)" != "666" ]
+then
+  echo "Permissions to $str_chirp_device not liberal enough. Trying with sudo to fix it."
+  sudo chmod 666 $str_chirp_device 
+fi
+
+# --volume /dev:/dev \
 podman run \
   --interactive \
   --tty \
   --env DISPLAY=$DISPLAY \
   --rm \
-  --volume /dev:/dev \
+  --volume $str_chirp_device:$str_chirp_device \
   --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
   --security-opt label=type:container_runtime_t \
   $str_image_name
+
